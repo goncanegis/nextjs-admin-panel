9fea23d9f2a5fef51f3c61dc6def04f6a48bb221
Gonca Negis <gonca.negis@gmail.com>
2022-07-09 14:51:58 +0300

    added ai writer filters

--- pages/ai-writer/posts/index.js
+++ pages/ai-writer/posts/index.js
@@ -22,6 +22,31 @@ const filtersData = [
     url: '#',
     color: '#0A84FF',
   },
+  {
+    title: 'Reklamlar',
+    url: '#',
+    color: '#FF375F',
+  },
+  {
+    title: 'Blog Yazımı',
+    url: '#',
+    color: '#AC8E68',
+  },
+  {
+    title: 'Satış & Pazarlama',
+    url: '#',
+    color: '#FFD60A',
+  },
+  {
+    title: 'E-Mail',
+    url: '#',
+    color: '#64D2FF',
+  },
+  {
+    title: 'SEO',
+    url: '#',
+    color: '#BF5AF2',
+  },
 ];
 
 export default function Posts() {
@@ -33,7 +58,7 @@ export default function Posts() {
 
       <section className={`m-3 mt-0 p-3 ${styles.mainSection}`}>
         <div className="d-flex">
-          <div className="filters-container me-3">
+          <div className="filters-container me-3 d-flex flex-direction-column justify-content-between">
             <div className="d-flex justify-content-between">
               <div className="filter select">
                 <p className="filter-label">Konu Başlığı</p>
@@ -55,7 +80,6 @@ export default function Posts() {
                       <div key={uuidv4()}>
                         <li>
                           <a className="dropdown-item" href={filter.url}>
-                            {/* <img src="/images/cartIcon.svg" alt="cart" /> */}
                             <CartIcon fill={filter.color || 'black'} />
                             {filter.title}
                           </a>
@@ -68,6 +92,256 @@ export default function Posts() {
                   </ul>
                 </div>
               </div>
+              <div className="filter select">
+                <p className="filter-label">Dil Seçimi</p>
+                <div className="dropdown">
+                  <button
+                    className="btn btn-dark dropdown-toggle"
+                    type="button"
+                    id="dropdownMenuButton2"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Konu Başlığı
+                  </button>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton2"
+                  >
+                    <li>
+                      <a className="dropdown-item" href={'#'}>
+                        Some Language
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href={'#'}>
+                        Another Language
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href={'#'}>
+                        Third Language
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div className="d-flex justify-content-between ">
+              <div className="filter select">
+                <p className="filter-label">Alt Başlık</p>
+                <div className="dropdown">
+                  <button
+                    className="btn btn-dark dropdown-toggle"
+                    type="button"
+                    id="dropdownMenuButton2"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Konu Başlığı
+                  </button>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton2"
+                  >
+                    <li>
+                      <a className="dropdown-item" href={'#'}>
+                        Some Language
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href={'#'}>
+                        Another Language
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href={'#'}>
+                        Third Language
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="filter select">
+                <p className="filter-label">Dil Tonu</p>
+                <div className="dropdown">
+                  <button
+                    className="btn btn-dark dropdown-toggle"
+                    type="button"
+                    id="dropdownMenuButton2"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Konu Başlığı
+                  </button>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton2"
+                  >
+                    <li>
+                      <a className="dropdown-item" href={'#'}>
+                        Some Language
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href={'#'}>
+                        Another Language
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href={'#'}>
+                        Third Language
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div className="d-flex ">
+              <div
+                className="filter textarea large w-100
+              "
+              >
+                <label
+                  htmlFor="exampleFormControlTextarea1"
+                  className="filter-label"
+                >
+                  Örnek Metin
+                </label>
+                <textarea
+                  id="exampleFormControlTextarea1
+                "
+                  placeholder="Konu Başlığı"
+                ></textarea>
+              </div>
+            </div>
+
+            <div className="d-flex ">
+              <div className="filter textarea small w-100">
+                <label
+                  htmlFor="exampleFormControlTextarea1"
+                  className="filter-label"
+                >
+                  Örnek Metin
+                </label>
+                <textarea
+                  id="exampleFormControlTextarea1
+                "
+                  placeholder="Konu Başlığı"
+                ></textarea>
+              </div>
+            </div>
+
+            <div className="d-flex justify-content-between">
+              <div className="filter select">
+                <p className="filter-label">Yaratıcılık Seçimi</p>
+                <div className="dropdown">
+                  <button
+                    className="btn btn-dark dropdown-toggle"
+                    type="button"
+                    id="dropdownMenuButton2"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Konu Başlığı
+                  </button>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton2"
+                  >
+                    <li>
+                      <a className="dropdown-item" href={'#'}>
+                        Some Language
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href={'#'}>
+                        Another Language
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href={'#'}>
+                        Third Language
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+              <div className="filter select">
+                <p className="filter-label">Varyant Sayısı</p>
+                <div className="dropdown">
+                  <button
+                    className="btn btn-dark dropdown-toggle"
+                    type="button"
+                    id="dropdownMenuButton2"
+                    data-bs-toggle="dropdown"
+                    aria-expanded="false"
+                  >
+                    Konu Başlığı
+                  </button>
+                  <ul
+                    className="dropdown-menu"
+                    aria-labelledby="dropdownMenuButton2"
+                  >
+                    <li>
+                      <a className="dropdown-item" href={'#'}>
+                        Some Language
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href={'#'}>
+                        Another Language
+                      </a>
+                    </li>
+                    <li>
+                      <hr className="dropdown-divider" />
+                    </li>
+                    <li>
+                      <a className="dropdown-item" href={'#'}>
+                        Third Language
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+
+            <div className="d-flex w-100 justify-content-center">
+              <button type="submit" className="btn result-button">
+                Sonuç Bul
+              </button>
             </div>
           </div>
 
